bfaa349f359d9c2f452d1634807b72bed420fd06
danbibibi <dlaeksql08222@gmail.com>
2022-01-26 03:51:06 +0900

    custom dataset

---
+++ custom_dataset.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import Dataset
+from PIL import Image, ImageOps
+
+class path_to_img(Dataset):
+    def __init__(self, img_path, labels, transform):  # 데이터셋 전처리
+        self.img_path = img_path
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):  # 데이터셋 길이 (총 샘플의 수)
+        return len(self.img_path)
+
+    def __getitem__(self, idx):  # 데이터셋에서 특정 샘플을 가져옴
+        image = ImageOps.grayscale(Image.open('.' + self.img_path.iloc[idx]))
+        image = self.transform(image)
+        return image, self.labels.iloc[idx]
