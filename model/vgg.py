4eae183f127de46d261acd1af00782ede5bf7319
danbibibi <dlaeksql08222@gmail.com>
2022-01-26 21:03:08 +0900

    vgg model

---
+++ model/vgg.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+
+class VGG16(nn.Module):
+
+    def __init__(self, input_channel, num_class):
+        super().__init__()
+        self.conv = nn.Sequential(
+            # 224 224 3 (입력)
+            # 224 224 64
+            nn.Conv2d(in_channels=input_channel, out_channels=64, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.Conv2d(in_channels=64 , out_channels=64 , kernel_size=3 , stride=1, padding=1), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # 112 112 128
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # 56 56 256
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # 28 28 512
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # 14 14 512
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2) # 7 7 512
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=512*7*7, out_features=4096), nn.ReLU(),
+            nn.Linear(in_features=4096, out_features=4096), nn.ReLU(),
+            nn.Linear(in_features=4096, out_features=num_class)
+        )
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        conv_out = self.conv(x)
+        conv_out = conv_out.view(conv_out.size()[0], -1) # 256, 512*7*7
+        fc_out = self.fc(conv_out)
+        output = self.softmax(fc_out)
+
+        return output
