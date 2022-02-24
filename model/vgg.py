c30b0c116899a8aa10ef56d0f5e614140d97bf85
danbibibi <dlaeksql08222@gmail.com>
2022-02-25 02:14:13 +0900

    vgg_Extractor

--- model/vgg.py
+++ model/vgg.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn as nn
+from torch import nn
 
 class VGG(nn.Module):
 
@@ -8,23 +8,25 @@ class VGG(nn.Module):
         self.conv = nn.Sequential(
             # 32 32 3 (입력)
             # 32 32 64
-            nn.Conv2d(in_channels=input_channel, out_channels=64, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=input_channel, out_channels=64, kernel_size=3, stride=1, padding=1), nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2), # 16 16 64
 
             # 16 16 128
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1), nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2), # 8 8 128
 
             # 8 8 256
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.BatchNorm2d(num_features=256), nn.ReLU(), # 
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.BatchNorm2d(num_features=256), nn.ReLU(), # 
             nn.MaxPool2d(kernel_size=2, stride=2), # 4 4 256
 
             # 4 4 512
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
-            nn.BatchNorm2d(num_features=512), nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
-            nn.BatchNorm2d(num_features=512), nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.BatchNorm2d(num_features=512), nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+            nn.BatchNorm2d(num_features=512), nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)  # 2 2 512
         )
 
